2c2bebcf3704b687a297fb3df00ea96b3382e5a5
rbs392 <rbs392@yahoo.com>
2016-11-24 03:34:29 +0530

    adding dynamic and better fetch for api

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
+import Url from 'url';
 import http from 'http';
+import https from 'https';
+import cheerio from 'cheerio';
 import express from 'express';
 
 const app = express();
@@ -10,11 +13,25 @@ app.use(express.static(`${__dirname}/app`));
 /* Api */
 app.get('/api/page', (request, response) => {
   const { url } = request.query;
-  const httpReq = http.get(url, (socket) => {
+  const urlObj = Url.parse(url);
+  const hostname = `${urlObj.protocol}//${urlObj.hostname}`;
+  const client = (urlObj.protocol === 'http:') ? http : https;
+  const httpReq = client.get(url, (socket) => {
     let data = '';
     socket.on('data', (chunk) => { data += chunk; });
     socket.on('end', () => {
-      response.status(200).json({ data });
+      const $ = cheerio.load(data);
+      ['src', 'href'].forEach((attr) => {
+        $(`[${attr}]`).each((i, el) => {
+          const val = $(el).attr(attr);
+          if (val && !(/(data)|(http)/.test(val))) {
+            // TODO: handle urls without / or .
+            const resolvedUrl = Url.resolve(hostname, val);
+            $(el).attr(attr, resolvedUrl);
+          }
+        });
+      });
+      response.status(200).json({ data: $('html').html() });
     });
     socket.on('error', (err) => {
       response.status(500).json({ err });
